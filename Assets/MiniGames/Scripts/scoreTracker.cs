18aae306a2cf21e4f22841c7da46c55bb62b6a0a
Sander <SaWIEn99@outlook.com>
2022-04-19 16:00:27 +0200

    Minigame UI
    UI for the minigames implemented, ready to be implemented to game proper

--- Assets/MiniGames/Scripts/scoreTracker.cs
+++ Assets/MiniGames/Scripts/scoreTracker.cs
@@ -1,25 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class scoreTracker : MonoBehaviour
 {
     public int part1, part2, part3, total;
+    public bool woodforking;
+    public GameObject UI;
+    [Header("On Stars")]
+    public RawImage star1, star2, star3;
+
+    [Header("Off Stars")]
+    public RawImage offStar2, offStar3;
+
+    [Header("UI Text")]
+    public string rewardName;
+    public TMP_Text rewardText;
+
+    public int scrappReward, woodReward;
+    int rewardType;
+    private void Start()
+    {
+        if (woodforking)
+        {
+            rewardType = woodReward;
+        }
+        else
+        {
+            rewardType = scrappReward;
+        }
+    }
     public void endScore()
     {
         total = part1 + part2 + part3;
         switch (total)
         {
             case >= 9:
-                print("3");
+                ThreeStar(3);
                 break;
             case >= 5:
-                print("2");
+                TwoStar(2);
                 break;
             default:
-                print("1");
+                OneStar(1);
                 break;
         }
-        
+
+    }
+    void OneStar(int stars)
+    {
+        UI.SetActive(true);
+        star1.enabled = true;
+        offStar2.enabled = true;
+        offStar3.enabled = true;
+        rewardText.text = "Reward: " + ((rewardType + Random.Range(-2, 3)) * stars) + " " + rewardName;
+    }
+    void TwoStar(int stars)
+    {
+        UI.SetActive(true);
+        star1.enabled = true;
+        star2.enabled = true;
+        offStar3.enabled = true;
+        rewardText.text = "Reward: " + ((rewardType + Random.Range(-2, 3)) * stars) + " " + rewardName;
+    }
+    void ThreeStar(int stars)
+    {
+        UI.SetActive(true);
+        star1.enabled = true;
+        star2.enabled = true;
+        star3.enabled = true;
+        rewardText.text = "Reward: " + ((rewardType + Random.Range(-2, 3)) * stars) + " " + rewardName;
     }
 }
