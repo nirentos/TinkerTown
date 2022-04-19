60443149aff7117ba486ac8a48f4ea2b378419cf
Jenny <jenny.g.torp@hotmail.com>
2022-04-19 21:02:50 +0200

    Birb & smok anims

---
+++ Assets/Sprites/Effects/_Scripts Effects/LightFlickerTimer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlickerTimer : MonoBehaviour
+{
+    public Animator anim;
+    public float timer;
+
+    void Start()
+    {
+        timer = Random.Range(5f, 12f);
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer < 0)
+        {
+            anim.SetTrigger("flicker");
+            timer = Random.Range(5f, 12f);
+        }
+    }
+}
