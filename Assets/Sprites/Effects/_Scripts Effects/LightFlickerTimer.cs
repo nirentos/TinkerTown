4a27591bd41fb3ebd7bacb61425da4ce673136dd
Jenny <jenny.g.torp@hotmail.com>
2022-04-20 23:38:02 +0200

    cutie poototie birbs

--- Assets/Sprites/Effects/_Scripts Effects/LightFlickerTimer.cs
+++ Assets/Sprites/Effects/_Scripts Effects/LightFlickerTimer.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class LightFlickerTimer : MonoBehaviour
 {
     public Animator anim;
-    public float timer;
+    public float timer, minTime, maxTime;
 
     void Start()
     {
-        timer = Random.Range(5f, 12f);
+        timer = Random.Range(minTime, maxTime);
     }
 
-    void Update()
+    private void Update()
     {
         timer -= Time.deltaTime;
-        if(timer < 0)
+        if (timer < 0)
         {
             anim.SetTrigger("flicker");
-            timer = Random.Range(anim.GetCurrentAnimatorStateInfo(0).length + 5f, 12f);
+            timer = Random.Range(minTime, maxTime);
         }
     }
 }
